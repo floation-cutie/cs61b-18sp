6ab21cb468d1725f6954fe49545524a65cd0a4b6
floation_cutie <2185368154@qq.com>
2023-07-22 02:54:25 +0800

    optimize the getRecursive method by avoiding nullpointerexpection and style change

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,60 +1,67 @@
-public class LinkedListDeque <Item> {
+public class LinkedListDeque<T> {
     /**
      * we use circular sentinels here,saving one.
-     * */
+     */
     private int size;
-    private ItemNode sentinel;
-    private class ItemNode{
-        Item val;
-        ItemNode prev;
-        ItemNode next;
-        private ItemNode(Item x) {
-        val = x;
+    private TNode sentinel;
+
+    private class TNode {
+        T val;
+        TNode prev;
+        TNode next;
+
+        private TNode(T x) {
+            val = x;
         }
     }
+
     /**
-     *  to create an empty LinkedListDeque
-     *  */
-    public LinkedListDeque(){
+     * to create an empty LinkedListDeque
+     */
+    public LinkedListDeque() {
         size = 0;
-        sentinel = new ItemNode(null); //null is available to generics
+        sentinel = new TNode(null); // null is available to generics
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
 
     /**
      * @source josh hug
-     * to create a "deep copy" of "other"!!!
-     * rather than just create a swallow copy,like in Python
-     * saying that : sentinel = other.sentinel; size = other.size;
-     * this will change the origin Object by making a subtle modulation on your cloned one!!
-     * first: to create an empty list,then addLast by iterative ways
-     * we can use <Item> in the beginning instead of casting
-     * */
-    public LinkedListDeque(LinkedListDeque other){
+     *         to create a "deep copy" of "other"!!!
+     *         rather than just create a swallow copy,like in Python
+     *         saying that : sentinel = other.sentinel; size = other.size;
+     *         this will change the origin Object by making a subtle modulation on
+     *         your cloned one!!
+     *         first: to create an empty list,then addLast by iterative ways
+     *         we can use <T> in the beginning instead of casting
+     *         we don't need to change the size at first!s
+     */
+    public LinkedListDeque(LinkedListDeque other) {
 
-        sentinel = new ItemNode(null);
+        sentinel = new TNode(null);
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
-        size = other.size();
-        for(int i = 0; i < size;i++){
-            addLast((Item) other.get(i));
+        for (int i = 0; i < size; i++) {
+            addLast((T) other.get(i));
         }
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
-        return (size==0)?true:false;
+    public boolean isEmpty() {
+        return (size == 0);
     }
 
     /* a little awkward here */
-    public void addFirst(Item item){
-        ItemNode t = new ItemNode(item);
-        if(isEmpty()){
-            sentinel.next = sentinel.prev = t;
-            t.prev = t.next = sentinel;
+    public void addFirst(T T) {
+        TNode t = new TNode(T);
+        if (isEmpty()) {
+            sentinel.next = t;
+            sentinel.prev = t;
+            t.prev = sentinel;
+            t.next = sentinel;
             size = 1;
             return;
         }
@@ -65,26 +72,29 @@ public class LinkedListDeque <Item> {
         size++;
     }
 
-    public void addLast(Item item)
-    {
-        ItemNode t = new ItemNode(item);
-        if(isEmpty()){
-            sentinel.next = sentinel.prev = t;
-            t.prev = t.next = sentinel;
+    public void addLast(T T) {
+        TNode t = new TNode(T);
+        if (isEmpty()) {
+            sentinel.next = t;
+            sentinel.prev = t;
+            t.prev = sentinel;
+            t.next = sentinel;
             size = 1;
             return;
         }
-        sentinel.prev.next = t;
         t.prev = sentinel.prev;
         t.next = sentinel;
+        sentinel.prev.next = t;
         sentinel.prev = t;
         size++;
     }
 
-    public Item removeFirst(){
-        if(isEmpty()) return null;
-        ItemNode tmp = sentinel.next;
-        Item returnValue = tmp.val;
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        TNode tmp = sentinel.next;
+        T returnValue = tmp.val;
         sentinel.next.next.prev = sentinel;
         sentinel.next = sentinel.next.next;
         tmp = null;
@@ -93,12 +103,14 @@ public class LinkedListDeque <Item> {
     }
 
     /**
-     * We won't maintain references to items that are no longer in the deque.
-     * */
-    public Item removeLast(){
-        if(isEmpty()) return null;
-        ItemNode tmp = sentinel.prev;
-        Item returnValue = tmp.val;
+     * We won't maintain references to Ts that are no longer in the deque.
+     */
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        TNode tmp = sentinel.prev;
+        T returnValue = tmp.val;
         sentinel.prev.prev.next = sentinel;
         sentinel.prev = sentinel.prev.prev;
         tmp = null;
@@ -106,36 +118,39 @@ public class LinkedListDeque <Item> {
         return returnValue;
     }
 
-    public Item get(int index){
-        if(index > size - 1) {
+    public T get(int index) {
+        if (index > size - 1) {
             return null;
         }
-        ItemNode ptr = sentinel;
+        TNode ptr = sentinel;
 
-        for(int i = 0;i <= index;i++){
+        for (int i = 0; i <= index; i++) {
             ptr = ptr.next;
         }
         return ptr.val;
     }
 
-    public Item getRecursive(int index){
-        if(index > size - 1) {
+    public T getRecursive(int index) {
+        if (index > size - 1) {
             return null;
         }
-        if(index == 0) return sentinel.next.val;
-        LinkedListDeque next_List = new LinkedListDeque(this);
-        next_List.removeFirst();
-        return (Item) next_List.getRecursive(index-1);
+        return getRecursiveHelper(index, sentinel.next);
     }
 
-    public void printDeque(){
-        ItemNode ptr = sentinel;
-        for(int i = 0; i < size; i++){
+    private T getRecursiveHelper(int index, TNode node) {
+        if (index == 0) {
+            return node.val;
+        }
+        return getRecursiveHelper(index - 1, node.next);
+    }
+
+    public void printDeque() {
+        TNode ptr = sentinel;
+        for (int i = 0; i < size; i++) {
             ptr = ptr.next;
-            if(i == size - 1){
+            if (i == size - 1) {
                 System.out.println(ptr.val);
-            }
-            else {
+            } else {
                 System.out.print(ptr.val + " ");
             }
         }
